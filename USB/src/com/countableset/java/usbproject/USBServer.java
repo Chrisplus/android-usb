013123526f0b6dd17d592e79b4af0cc107f53a68
Rachel Aurand <rachel.aurand@gmail.com>
2011-11-03 22:47:34 -0700

    flush bug still not fixed

--- USB/src/com/countableset/java/usbproject/USBServer.java
+++ USB/src/com/countableset/java/usbproject/USBServer.java
@@ -6,24 +6,45 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Date;
 import java.util.Scanner;
 
 public class USBServer {
 
+	public static Socket echoSocket = null;
+	public static PrintStream out = null;
+	public static BufferedReader in = null;
+	public static String line = null;
+	public static boolean send = true;
+	private static int portNumber = 38301;
+	
 	public static void main(String[] args) throws IOException {
 
 		System.out.println("Connection.main()");
 		
 		execAdb();
 		
-		Socket echoSocket = null;
-		PrintStream out = null;
-		BufferedReader in = null;
+		connect();
+	
+		for(int i = 0; i <= 10; i++) {
+			out.println("List Space: " + i);
+			out.flush();
+		}
+		out.flush();
 
+		
+		while((line = in.readLine()) != null) {
+			System.out.println("Server> " + line);
+		}
+		
+		disconnect();
+		
+	} // end main()
+	
+	
+	private static void connect() {
 		try {
-			echoSocket = new Socket("localhost", 38300);
-			out = new PrintStream(echoSocket.getOutputStream());
+			echoSocket = new Socket("localhost", portNumber);
+			out = new PrintStream(echoSocket.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(
 					echoSocket.getInputStream()));
 		} catch (UnknownHostException e) {
@@ -34,47 +55,19 @@ public class USBServer {
 					+ "the connection to: localhost.");
 			System.exit(1);
 		}
-
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		// String userInput;
-		System.out.println("connected!!");
-
-//		int counter = 0;
-//		TODO monitor
-//		while (true) {
-//			counter++;
-//			// out.println(counter);
-//			if (counter % 1000 == 0) {
-//				out.println("update" + new Date().getSeconds());
-//				counter = 1;
-//				System.out.println("echo: " + in.readLine());
-//			}
-//		}
-		
-//		String line = null;
-//		while((line = in.readLine()) != null) {
-//			System.out.println("Got something!: " + line);
-//		}
-		
-		for(int i = 0; i <= 5; i++) {
-			out.println("List Space: " + i);
-		}
-		
-//		out.println("List Space 1");
-//		out.println("List Space 2");
-//		out.println("List Space 3");
-//		
-//		out.println("testing output to input");
-		
+		System.out.println("connected!");
+	}
+	
+	private static void disconnect() throws IOException {
 		out.close();
 		in.close();
 		echoSocket.close();
-	} // end main()
+	}
 	
 	private static void execAdb() {
 		// run the adb bride
 		try {
-			Process p = Runtime.getRuntime().exec("/Users/rachel/Documents/sdev/android-sdk/platform-tools/adb forward tcp:38300 tcp:38300");
+			Process p = Runtime.getRuntime().exec("/Users/rachel/Documents/sdev/android-sdk/platform-tools/adb forward tcp:" + portNumber + " tcp:" + portNumber );
 			Scanner sc = new Scanner(p.getErrorStream());
 			if(sc.hasNext()) {
 				while(sc.hasNext()) System.out.println(sc.next());
--- USBSampleProject/src/com/countableset/android/usbsampleproject/USBProject.java
+++ USBSampleProject/src/com/countableset/android/usbsampleproject/USBProject.java
@@ -12,10 +12,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class USBProject extends Activity implements OnClickListener {
@@ -39,6 +45,8 @@ public class USBProject extends Activity implements OnClickListener {
 	private Handler mHandler = null;
 	private ServerSocket server = null;
 	private Socket client = null;
+	private String selectedItem = null;
+	private int portNumber = 38301;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,32 @@ public class USBProject extends Activity implements OnClickListener {
 
 		mHandler = new Handler();
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.connected_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case R.id.refresh:
+				return true;
+			case R.id.back:
+				setContentView(R.layout.main);
+
+				//Set up click listeners for the buttons
+				View connectButton = findViewById(R.id.connect_button);
+				connectButton.setOnClickListener(this);
+
+				mHandler = new Handler();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	} // onOptionsItemSlected()
 
 	/**
 	 * The function that deals with the button click to connect via usb
@@ -79,7 +113,7 @@ public class USBProject extends Activity implements OnClickListener {
 		public void run() {
 			// initialize server socket
 			try {
-				server = new ServerSocket(38300);
+				server = new ServerSocket(portNumber);
 				Log.d(TAG, "waiting for connection");
 				while(true) {
 					//listen for incoming clients
@@ -87,6 +121,12 @@ public class USBProject extends Activity implements OnClickListener {
 					try {
 						Globals.socketIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
 						Globals.socketOut = new PrintWriter(client.getOutputStream(), true);
+						
+						Globals.socketOut.println("Hello from the server!");
+						
+						
+//						new Thread(getData).start();
+						
 						String line = null;
 						stringList = new ArrayList<String>();
 						// reading in the input from the usb socket
@@ -95,6 +135,8 @@ public class USBProject extends Activity implements OnClickListener {
 						}
 						// show the input from usb as a list view in the activity
 						mHandler.post(showListView);
+
+						
 					} catch (Exception e) {
 						Log.d(TAG, "lost client");
 						e.printStackTrace();
@@ -109,6 +151,31 @@ public class USBProject extends Activity implements OnClickListener {
 		} // end run()
 	};
 	
+	private Runnable getData = new Thread() {
+		public void run() {
+			try {
+				String line = null;
+				stringList = new ArrayList<String>();
+				// reading in the input from the usb socket
+				while((line = Globals.socketIn.readLine()) != null) {
+					stringList.add(line);
+				}
+				// show the input from usb as a list view in the activity
+				mHandler.post(showListView);
+			} catch (Exception e) {
+				Log.d(TAG, "error in getData");
+			}
+		}
+	};
+	
+	private Runnable sendData = new Thread() {
+		public void run() {
+			Log.d(TAG, "send data " + selectedItem);
+			Globals.socketOut.println(selectedItem);
+		}
+	};
+	
+	
 	/**
 	 * Shows a list view of of the input from socketIn
 	 * called in initializeConnection thread
@@ -119,6 +186,16 @@ public class USBProject extends Activity implements OnClickListener {
 			ListView lv = new ListView(USBProject.this);
 			lv.setAdapter(new ArrayAdapter<String>(USBProject.this, R.layout.connected, stringList));
 			setContentView(lv);
+			
+			lv.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					// When clicked, send the text
+					//Globals.socketOut.println(((TextView) view).getText());
+					selectedItem = (String) ((TextView) view).getText();
+					Log.d(TAG, "The selected item is (on listener): " + selectedItem);
+					new Thread(sendData).start();
+				}
+			});
 		}
 	};
 
